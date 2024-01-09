5bdd5bf0d14ec020e32d0732401a828d536da462
darlingtech <dalintymoses@gmail.com>
2024-01-09 14:16:08 +0100

    my first commit

---
+++ techclass1.py
@@ -0,0 +1,20 @@
+# Given variables
+num1 = 7
+num2 = 2.5
+text= "greetings"
+total = num1+num2
+print(total)
+caption = "book"
+result = text + caption
+print(result)
+conclusion = num1*4
+print(conclusion)
+expression = (num1 + num2) *3
+print(expression)
+result_integer = num1 / num2
+print(result_integer)
+result = num1 ** 2
+print(result)
+result = num1 % 2
+print(result)
+print
